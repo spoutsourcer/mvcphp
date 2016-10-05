cd41d4bcb503e756d1a42c668bf47209758cc5cd
MD. Sohel Parvez <sohel.parvez@spectru-bd.com>
2016-10-05 12:05:41 +0600

    committed to master.

--- application/api/v1/config.php
+++ application/api/v1/config.php
@@ -3,7 +3,7 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'lalcoden_userportal');
 
